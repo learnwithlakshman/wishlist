01e58f82d81bb0de28797d8645c502d1fb30e8c1
M Rajesh <m.rajeshrajz@gmail.com>
2020-05-19 18:33:52 +0530

    updated

--- src/main/java/com/lwl/wishlist/web/CommonController.java
+++ src/main/java/com/lwl/wishlist/web/CommonController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class CommonController {
 	
-	@GetMapping
+	@GetMapping("/")
 	public String redirect() {
 		return "redirect:wl/home";
 	}
